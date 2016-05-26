416ddb1a34603911689d8a3d3468a38a589ee55a
Gerald Soriano <sorianog@msoe.edu>
2016-05-26 12:09:30 -0500

    Completed Part 6 of tutorial. Created the Bolt prefab.

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+    private Rigidbody rigBod;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        rigBod = GetComponent<Rigidbody>();
+        rigBod.velocity = transform.forward * speed;
+	}
+}
