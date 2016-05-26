b0bf8017311c52ea7aa6e2f97999ba480e8703f6
Gerald Soriano <sorianog@msoe.edu>
2016-05-26 11:46:32 -0500

    Completed Part 5 of tutorial. Added a script to control the ship's movements.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rigBod;
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+    void Start()
+    {
+        rigBod = GetComponent<Rigidbody>(); 
+    }
+
+    void FixedUpdate()
+    {
+        float moveHort = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHort, 0.0f, moveVert);
+        rigBod.velocity = movement * speed;
+
+        rigBod.position = new Vector3
+        (
+            Mathf.Clamp(rigBod.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rigBod.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rigBod.rotation = Quaternion.Euler(0.0f, 0.0f, rigBod.velocity.x * -tilt);
+    }
+}
