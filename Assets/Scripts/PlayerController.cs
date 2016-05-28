995e6f6f1363142870846857e68397c6e7a5eaa3
Gerald Soriano <sorianog@msoe.edu>
2016-05-28 10:36:23 -0500

    Completed Part 13 of tutorial. Added sound effects and background music.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -31,6 +31,7 @@ public class PlayerController : MonoBehaviour {
         {
             nextFire = Time.time + fireRate;
             Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            GetComponent<AudioSource>().Play();
         }
     }
 
