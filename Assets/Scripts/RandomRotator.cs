06a54cb46012169ba363784f6be9a79a70f6aaa9
Gerald Soriano <sorianog@msoe.edu>
2016-05-27 21:14:54 -0500

    Completed Part 9 of tutorial. Added an asteroid to the scene that can be destroyed by a lazer.

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+
+    private Rigidbody rigBod;
+    public float tumble;
+
+	// Use this for initialization
+	void Start () {
+        rigBod = GetComponent<Rigidbody>();
+        rigBod.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
